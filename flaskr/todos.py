419f3c3f1c55e9e56080d7f1811a09dcf0e29d13
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2024-03-04 23:36:51 -0500

    add index view controller

--- flaskr/todos.py
+++ flaskr/todos.py
@@ -7,3 +7,19 @@ from flaskr.auth import login_required
 from flask.db import get_db
 
 bp = Blueprint('todos', __name__)
+
+
+@bp.route('/index', methods=('GET', 'POST', 'UPDATE'))
+def index():
+    if request.method == 'GET':
+        if g.user is None:
+            return render_template('index.html')
+
+        db = get_db()
+        todos = db.execute(
+            'SELECT t.body, status FROM todo t JOIN user u ON t.user_id = u.id'
+        ).fetchall()
+        return redirect(url_for('todos/todos.html', todos=todos))
+
+    if request.method == 'POST':
+         GF
