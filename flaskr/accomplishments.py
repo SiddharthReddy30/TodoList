cd397864c417e88abfe309891b9db7ca25d4b5cd
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2024-04-01 21:54:10 -0400

    Implement index view

--- flaskr/accomplishments.py
+++ flaskr/accomplishments.py
@@ -12,3 +12,9 @@ bp = Blueprint('accomplishments', __name__)
 @bp.route('/')
 @login_required
 def index():
+    db = get_db()
+    accomps = db.execute(
+            ' SELECT t.id,body, status, user_id'
+            'FROM todo t JOIN user u ON t.user_id = u.id'
+    ).fetchall()
+    return render_template('accomplshiments/index.html', accomps= accomps)
